4cf62113a2a53567d1f84731b85abc06642641ea
Bikash Adhikari <bikash30851@gmail.com>
2021-10-27 20:36:46 -0500

    made changes to users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
+var val = 0;
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  val++;
+  res.send('User accesses are: ' + val);
 });
 
 module.exports = router;
